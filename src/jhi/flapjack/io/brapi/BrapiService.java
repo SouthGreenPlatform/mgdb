5992495b5a1a7e50453497b35cc33342c7537b45
sempere <guilhem.sempere@cirad.fr>
2017-09-08 11:48:18 +0200

    Fixed minor issues

--- src/jhi/flapjack/io/brapi/BrapiService.java
+++ src/jhi/flapjack/io/brapi/BrapiService.java
@@ -35,19 +35,26 @@ public interface BrapiService
 	@GET("maps/{id}/positions")
 	Call<BrapiListResource<BrapiMarkerPosition>> getMapMarkerData(@Path("id") String id, @Query("pageSize") String pageSize, @Query("page") String page);
 
+	@GET("markers")
+	Call<BrapiListResource<BrapiMarker>> getMarkerInfo(@Query("name") Set<String> name, @Query("matchMethod") String matchMethod, @Query("include") String include, @Query("type") String type, @Query("pageSize") String pageSize, @Query("page") String page);
+
 	@FormUrlEncoded
 	@POST("markers")
-	Call<BrapiListResource<BrapiMarker>> getMarkerInfo(@Field("name") Set<String> name, @Field("matchMethod") String matchMethod, @Field("include") String include, @Field("type") String type, @Field("pageSize") String pageSize, @Field("page") String page);
+	Call<BrapiListResource<BrapiMarker>> getMarkerInfo_byPost(@Field("name") Set<String> name, @Field("matchMethod") String matchMethod, @Field("include") String include, @Field("type") String type, @Field("pageSize") String pageSize, @Field("page") String page);
 
 	@GET("markerprofiles")
 	Call<BrapiListResource<BrapiMarkerProfile>> getMarkerProfiles(@Query("studyDbId") String studyDbId, @Query("germplasm") Collection<String> germplasmDbIDs, @Query("pageSize") String pageSize, @Query("page") String page);
 
+//	@FormUrlEncoded
+//	@POST(value="allelematrix-search")
+//	Call<BrapiBaseResource<BrapiAlleleMatrix>> getAlleleMatrixById(@Field(value="matrixDbId") String matrixDbId, @Field("format") String format, @Field("expandHomozygotes") Boolean expandHomozygotes, @Field("unknownString") String unknownString, @Field("sepPhased") String sepPhased, @Field("sepUnphased") String sepUnphased, @Field("pageSize") Integer pageSize, @Field(value="page") Integer page);
+	
 	@GET("allelematrix-search")
 	Call<BrapiBaseResource<BrapiAlleleMatrix>> getAlleleMatrix(@Query("markerprofileDbId") List<String> markerProfileDbIds, @Query("markerDbId") List<String> markerDbIds, @Query("format") String format, @Query("expandHomozygotes") Boolean expandHomozygotes, @Query("unknownString") String unknownString, @Query("sepPhased") String sepPhased, @Query("sepUnphased") String sepUnphased, @Query("pageSize") String pageSize, @Query("page") String page);
 
 	@FormUrlEncoded
 	@POST("allelematrix-search")
-	Call<BrapiBaseResource<BrapiAlleleMatrix>> getAlleleMatrix_byPost(@Field("markerprofileDbId") List<String> markerProfileDbIds, @Field("markerDbId") List<String> markerDbIds, @Field("format") String format, @Field("expandHomozygotes") Boolean expandHomozygotes, @Field("unknownString") String unknownString, @Field("sepPhased") String sepPhased, @Field("sepUnphased") String sepUnphased, @Field("pageSize") String pageSize, @Field("page") String page);
+	Call<BrapiBaseResource<BrapiAlleleMatrix>> getAlleleMatrix_byPost(@Field("markerprofileDbId") List<String> markerProfileDbIds, 	@Field("markerDbId") List<String> markerDbIds,	@Field("format") String format, @Field("expandHomozygotes") Boolean expandHomozygotes, @Field("unknownString") String unknownString, @Field("sepPhased") String sepPhased, @Field("sepUnphased") String sepUnphased, @Field("pageSize") String pageSize, @Field("page") String page);
 	
 	@GET("allelematrix-search/status/{id}")
 	Call<BrapiBaseResource<Object>> getAlleleMatrixStatus(@Path("id") String extractId);
